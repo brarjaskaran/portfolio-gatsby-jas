0e24c4dbdff0f6979f2f519a510eb05b0f9bafef
Jaskaran Brar <jaskaranbrar77@gmail.com>
2021-02-02 00:11:36 +1030

    added footer@projets

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby';
-import Header from 'components/Header';
 import  Posts  from 'components/Projects/Posts';
+import  Layout  from 'components/Layout';
 
 const projects: React.FC = () => {
     return (
-        <div>
-            <Header/>
-            <Posts/>
-        </div>
+        <Layout>
+            <Posts />
+        </Layout>
     )
 }
 
